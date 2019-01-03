eea7eab294a524d1ae7369c82841f6fab0da0164
unknown <yctchung@gmail.com>
2019-01-03 15:48:56 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -43,11 +43,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (event) {
     event.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-        //xxx;
+        messageTextbox.val('')
     });
 });
 
@@ -57,13 +59,17 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
         //console.log(position);
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
