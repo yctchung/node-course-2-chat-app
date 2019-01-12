006bfca3f6a763c9a3a8343e20d16c5aa3630885
unknown <yctchung@gmail.com>
2019-01-12 16:38:58 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 /* The arrow function works in Chrome, but it
 will fail on mobile phone, Internet Explorer, 
 Safari, or some versions of Firefox.
@@ -39,6 +56,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -63,6 +81,7 @@ socket.on('newLocationMessage', function (message) {
    });
 
    jQuery('#messages').append(html);
+   scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (event) {
